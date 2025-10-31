c57dff5ce56e07384dd40e77c4911786524fee62
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2025-10-31 17:23:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -3,14 +3,9 @@ class Solution {
     int kthSmallest(vector<int> &arr, int k) {
         // code here
         priority_queue<int,vector<int>>maxheap;
-        for(int i=0;i<k;i++){
-            maxheap.push(arr[i]);
-        }
-        for(int i=k;i<arr.size();i++){
-            if(arr[i]<maxheap.top()){
-                maxheap.pop();
-                maxheap.push(arr[i]);
-            }
+        for(auto &num:arr){
+            maxheap.push(num);
+            if(maxheap.size()>k) maxheap.pop();
         }
         return maxheap.top();
     }
