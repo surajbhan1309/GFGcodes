de24574d04ed0e2d8a0da61c245505ab2cd52171
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-06-26 12:15:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
+++ Difficulty: Medium/Maximum Sum Combination/maximum-sum-combination.cpp
@@ -1,39 +1,111 @@
 class Solution {
-  public:
+public:
     vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
-        // code here
-        priority_queue<pair<int,pair<int,int>>> maxheap;
-        int n=a.size();
-        sort(a.rbegin(),a.rend());
-        sort(b.rbegin(),b.rend());
-        set<pair<int,int>>visited;
-        
-        maxheap.push({a[0]+b[0],{0,0}});
-        visited.insert({0,0});
-        
-        vector<int>result;
-        
-        while(k-- && !maxheap.empty()){
-            auto top=maxheap.top();
+        priority_queue<tuple<int, int, int>> maxheap;
+        int n = a.size();
+
+        sort(a.rbegin(), a.rend());
+        sort(b.rbegin(), b.rend());
+
+        set<pair<int, int>> visited;
+
+        maxheap.push({a[0] + b[0], 0, 0});
+        visited.insert({0, 0});
+
+        vector<int> result;
+
+        while (k-- && !maxheap.empty()) {
+            auto [sum, i, j] = maxheap.top();
             maxheap.pop();
-            
-            int sum=top.first;
-            int i=top.second.first;
-            int j=top.second.second;
-            
+
             result.push_back(sum);
-            
-            //next possible 
-            if(i+1<n && visited.find({i+1,j})==visited.end()){
-                maxheap.push({a[i+1]+b[j],{i+1,j}});
-                visited.insert({i+1,j});
+
+            if (i + 1 < n && visited.find({i + 1, j}) == visited.end()) {
+                maxheap.push({a[i + 1] + b[j], i + 1, j});
+                visited.insert({i + 1, j});
             }
-            if(j+1<n && visited.find({i,j+1})==visited.end()){
-                maxheap.push({a[i]+b[j+1],{i,j+1}});
-                visited.insert({i,j+1});
+
+            if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+                maxheap.push({a[i] + b[j + 1], i, j + 1});
+                visited.insert({i, j + 1});
             }
-           
         }
+
         return result;
     }
 };
+// class Solution {
+// public:
+//     vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+//         priority_queue<tuple<int, int, int>> maxheap;
+//         int n = a.size();
+
+//         sort(a.rbegin(), a.rend());
+//         sort(b.rbegin(), b.rend());
+
+//         set<pair<int, int>> visited;
+
+//         maxheap.push({a[0] + b[0], 0, 0});
+//         visited.insert({0, 0});
+
+//         vector<int> result;
+
+//         while (k-- && !maxheap.empty()) {
+//             auto [sum, i, j] = maxheap.top();
+//             maxheap.pop();
+
+//             result.push_back(sum);
+
+//             if (i + 1 < n && visited.find({i + 1, j}) == visited.end()) {
+//                 maxheap.push({a[i + 1] + b[j], i + 1, j});
+//                 visited.insert({i + 1, j});
+//             }
+
+//             if (j + 1 < n && visited.find({i, j + 1}) == visited.end()) {
+//                 maxheap.push({a[i] + b[j + 1], i, j + 1});
+//                 visited.insert({i, j + 1});
+//             }
+//         }
+
+//         return result;
+//     }
+// };
+// class Solution {
+//   public:
+//     vector<int> topKSumPairs(vector<int>& a, vector<int>& b, int k) {
+//         // code here
+//         priority_queue<pair<int,pair<int,int>>> maxheap;
+//         int n=a.size();
+//         sort(a.rbegin(),a.rend());
+//         sort(b.rbegin(),b.rend());
+//         set<pair<int,int>>visited;
+        
+//         maxheap.push({a[0]+b[0],{0,0}});
+//         visited.insert({0,0});
+        
+//         vector<int>result;
+        
+//         while(k-- && !maxheap.empty()){
+//             auto top=maxheap.top();
+//             maxheap.pop();
+            
+//             int sum=top.first;
+//             int i=top.second.first;
+//             int j=top.second.second;
+            
+//             result.push_back(sum);
+            
+//             //next possible 
+//             if(i+1<n && visited.find({i+1,j})==visited.end()){
+//                 maxheap.push({a[i+1]+b[j],{i+1,j}});
+//                 visited.insert({i+1,j});
+//             }
+//             if(j+1<n && visited.find({i,j+1})==visited.end()){
+//                 maxheap.push({a[i]+b[j+1],{i,j+1}});
+//                 visited.insert({i,j+1});
+//             }
+           
+//         }
+//         return result;
+//     }
+// };
