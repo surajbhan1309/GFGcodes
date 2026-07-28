09119627d498f1001fc870896b66265e42ab0b73
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-07-28 16:32:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -2,32 +2,31 @@ class Solution {
   public:
     int spanningTree(int V, vector<vector<int>>& edges) {
         vector<pair<int,int>>adj[V];
-        for(auto &e:edges){
-            int u=e[0];
-            int v=e[1];
-            int w=e[2];
-            
-            adj[u].push_back({v,w});
-            adj[v].push_back({u,w});
+        for(auto e:edges){
+            adj[e[0]].push_back({e[1],e[2]});
+            adj[e[1]].push_back({e[0],e[2]});
         }
-        vector<int>dist(V,1e9);
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>dist(V,1e9);
         vector<int>vis(V,0);
         pq.push({0,0});
         int sum=0;
         
         while(!pq.empty()){
-            auto [wt,node]=pq.top();
+            auto[wt,node]=pq.top();
             pq.pop();
+            
             if(vis[node]) continue;
             vis[node]=1;
             sum+=wt;
-            for(auto &[nbr,w]:adj[node]){
+            
+            for(auto [nbr,weight]:adj[node]){
                 if(!vis[nbr]){
-                    pq.push({w,nbr});
+                    pq.push({weight,nbr});
                 }
             }
         }
         return sum;
+        
     }
 };
