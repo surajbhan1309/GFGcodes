cb8e6e3a8fff411b3df51139187600c5bf9f2ed0
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-08-04 22:12:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Meeting Rooms/meeting-rooms.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    bool canAttend(vector<vector<int>> &arr) {
+        sort(arr.begin(),arr.end());
+        int n=arr.size();
+        for(int i=1;i<n;i++){
+            if(arr[i-1][1]>arr[i][0]){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
