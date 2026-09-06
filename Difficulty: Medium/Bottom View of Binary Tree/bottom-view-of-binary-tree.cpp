c81e8390e11c5c4ecbfc3b427f573996ca5ff8c4
Suraj Bhan Majhi <151545643+surajbhan1309@users.noreply.github.com>
2026-09-06 16:19:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
+++ Difficulty: Medium/Bottom View of Binary Tree/bottom-view-of-binary-tree.cpp
@@ -17,22 +17,16 @@ class Solution {
   public:
     vector<int> bottomView(Node *root) {
         vector<int>ans;
-        map<int,int>mp;
         queue<pair<Node*,int>>q;
         q.push({root,0});
-        if(!root) return ans;
+        map<int,int>mp;
         while(!q.empty()){
-            auto [node,hd]=q.front();
+            auto &[node,hd]=q.front();
             q.pop();
             
             mp[hd]=node->data;
-            
-            if(node->left){
-                q.push({node->left,hd-1});
-            }
-            if(node->right){
-                q.push({node->right,hd+1});
-            }
+            if(node->left) q.push({node->left,hd-1});
+            if(node->right) q.push({node->right,hd+1});
         }
         for(auto &x:mp){
             ans.push_back(x.second);
